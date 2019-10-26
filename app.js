fdb20829f58ab61932973f13cb1266a71b1eacb7
F0rl <F0rL@users.noreply.github.com>
2019-10-27 00:42:14 +0800

    get  api

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = require('http')
+const querystring = require('querystring')
+
+// http://localhost:3000/api/bloglist?id=123456&name=kuma
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'content-type': 'text/html'})
-  res.end('<h1>hello kuma</h1>')
+  console.log(req.method)
+  const url = req.url
+  console.log(url)
+  req.query = querystring.parse(url.split('?')[1])
+  console.log(req.query);
+  res.end(
+    JSON.stringify(req.query)
+  )
 })
 
-server.listen(3000,() => {
+server.listen(3000, () => {
   console.log('listening on 3000 port');
 })
