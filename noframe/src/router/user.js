f711e0bf780766d12369bd3b683be8c0ae5455d9
F0rl <F0rL@users.noreply.github.com>
2019-10-28 21:35:19 +0800

    cookie 获取和设置

--- noframe/src/router/user.js
+++ noframe/src/router/user.js
@@ -1,6 +1,14 @@
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+const getCookieExpires = () => {
+  const d = new Date()
+  // console.log(d.getTime()+(24 * 60 * 60 * 1000));
+  // d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  // return d.toGMTString()
+  return new Date(d.getTime() + (24 * 60 * 60 * 1000))
+}
+
 const handleUserRouter = (req, res) => {
   const { method, path } = req
   // 登录
@@ -11,20 +19,20 @@ const handleUserRouter = (req, res) => {
     return result.then(data => {
       if (data.username) {
         // 操作cookie
-        res.setHeader('Set-Cookie', `username=${req.query.username};path=/`)
+        res.setHeader('Set-Cookie', `username=${req.query.username};path=/;httpOnly;expires=${getCookieExpires()}`)
         return new SuccessModel('登录成功')
       } else {
         return new ErrorModel('登录失败')
       }
     })
   }
-    // 登录验证测试
-  if(method === 'GET' && path === '/api/user/login-test') {
-    if(req.cookie.username) {
+  // 登录验证测试
+  if (method === 'GET' && path === '/api/user/login-test') {
+    if (req.cookie.username) {
       return Promise.resolve(new SuccessModel({
         username: req.cookie.username
       }))
-    }else {
+    } else {
       return Promise.resolve(new ErrorModel('尚未登录'))
     }
   }
