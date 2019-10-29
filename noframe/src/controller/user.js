2983bae546d363b3fde0fa8c4b08384c9f0debbe
F0rl <F0rL@users.noreply.github.com>
2019-10-29 22:33:17 +0800

    sql注入 注意拼接sql语句

--- noframe/src/controller/user.js
+++ noframe/src/controller/user.js
@@ -1,8 +1,12 @@
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
 const login = (username, password) => {
+  // 避免像括号内内容{kuma' -- }sql注入
+  username = escape(username)
+  password = escape(password)
   const sql = `
-    select username, realname from users where username='${username}' and password='${password}'
+    select username, realname from users where username=${username} and password=${password}
   `
+  // console.log('sql', sql);
   return exec(sql).then(rows => {
     return rows[0] || {}
   })
