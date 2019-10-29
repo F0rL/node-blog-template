8b1555cd8769f1227c87a8e8a392e009441e5cd6
F0rl <F0rL@users.noreply.github.com>
2019-10-30 00:18:40 +0800

    密码加密

--- noframe/src/controller/user.js
+++ noframe/src/controller/user.js
@@ -1,7 +1,13 @@
 const { exec, escape } = require('../db/mysql')
+const {genPassword} = require('../utils/cryp')
+
 const login = (username, password) => {
   // 避免像括号内内容{kuma' -- }sql注入
   username = escape(username)
+
+  // 加密密码
+  password = genPassword(password)
+
   password = escape(password)
   const sql = `
     select username, realname from users where username=${username} and password=${password}
