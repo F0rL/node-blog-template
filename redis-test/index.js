19a2a65ea6129ecbef3427c666f831f651e1fa21
F0rl <F0rL@users.noreply.github.com>
2019-10-29 10:03:13 +0800

    redis demo

---
+++ redis-test/index.js
@@ -0,0 +1,19 @@
+const redis = require('redis')
+
+
+const redisClient = redis.createClient(6379, '127.0.0.1')
+
+redisClient.on('error', err => {
+  console.error(err);
+})
+
+
+redisClient.set('username', 'never', redis.print)
+redisClient.get('username', (err, val) => {
+  if(err) {
+    console.error(err)
+    return
+  }
+  console.log('val', val);
+  redisClient.quit()
+})
