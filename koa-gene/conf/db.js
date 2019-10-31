cd275b0252e74100d0be34fb41a5d218446c79b3
F0rl <F0rL@users.noreply.github.com>
2019-10-31 17:12:26 +0800

    koa 模板

---
+++ koa-gene/conf/db.js
@@ -0,0 +1,39 @@
+const env = process.env.NODE_ENV
+
+let MYSQL_CONF
+let REDIS_CONF
+
+if (env === 'development') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'a3251520',
+    port: '3306',
+    database: 'blog_test'
+  }
+
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
+}
+
+if (env === 'production') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'a3251520',
+    port: '3306',
+    database: 'blog_test'
+  }
+
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
+}
+
+module.exports = {
+  MYSQL_CONF,
+  REDIS_CONF
+}
---
+++ koa-gene/utils/cryp.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto')
+
+// 秘钥
+const SECRET_KEY = 'Xx110_120#'
+
+function md5(content) {
+  let md5 = crypto.createHash('md5')
+  return md5.update(content).digest('hex')
+}
+
+function genPassword(password) {
+  const str = `password=${password}&key=${SECRET_KEY}`
+  return md5(str)
+}
+
+module.exports = {
+  genPassword
+}
